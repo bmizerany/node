bdfe84f9650bd6088ffc2bad048fe7250913d23a
Blake Mizerany <blake.mizerany@gmail.com>
2010-05-17 15:23:50 -0700

    trying to break this

--- test/simple/test-stdout-to-file.js
+++ test/simple/test-stdout-to-file.js
@@ -2,9 +2,10 @@ require('../common');
 var path = require('path')
   , childProccess = require('child_process')
   , fs = require('fs')
-  , stdoutScript = path.join(path.dirname(__dirname), 'fixtures/stdout.js')
+  , stdoutScript = path.join(path.dirname(__dirname), 'fixtures/print-chars.js')
   , tmpFile = path.join(path.dirname(__dirname), 'fixtures/stdout.txt')
-  , cmd = process.argv[0]+' '+stdoutScript+' > '+tmpFile;
+  , nChars = 1024 * 100
+  , cmd = process.argv[0]+' '+stdoutScript+' '+nChars+' > '+tmpFile;
 
 try {
   fs.unlinkSync(tmpFile);
@@ -14,6 +15,6 @@ childProccess.exec(cmd, function(err) {
   if (err) throw err;
 
   var data = fs.readFileSync(tmpFile);
-  assert.equal(data, "test\n");
+  assert.equal(nChars, data.length);
   fs.unlinkSync(tmpFile);
 });
