c88fbd6ad9bf13736cf16ed0ffe41d029b6805d6
Blake Mizerany <blake.mizerany@gmail.com>
2010-06-30 00:20:49 -0700

    fk unneeded semicolons

--- benchmark/run.js
+++ benchmark/run.js
@@ -1,31 +1,33 @@
-var path = require("path");
-var sys = require("sys");
-var childProcess = require("child_process");
-var benchmarks = [ "timers.js"
-                 , "process_loop.js"
-                 , "static_http_server.js"
-                 ];
+var path = require("path")
+var sys = require("sys")
+var childProcess = require("child_process")
 
-var benchmarkDir = path.dirname(__filename);
+var benchmarkDir = path.dirname(__filename)
+var dirContents = require("fs").readdirSync(benchmarkDir)
+
+var benchmarks = dirContents.filter(function (filename) {
+  return filename.match(/^bench_.*\.js$/)
+})
 
 function exec (script, callback) {
-  var start = new Date();
-  var child = childProcess.spawn(process.argv[0], [path.join(benchmarkDir, script)]);
+  var start = new Date()
+  var child = childProcess.spawn(process.argv[0], [path.join(benchmarkDir, script)])
   child.addListener("exit", function (code) {
-    var elapsed = new Date() - start;
-    callback(elapsed, code);
-  });
+    var elapsed = new Date() - start
+    callback(elapsed, code)
+  })
 }
 
 function runNext (i) {
-  if (i >= benchmarks.length) return;
-  sys.print(benchmarks[i] + ": ");
+  if (i >= benchmarks.length) return
+  sys.print(benchmarks[i] + ": ")
   exec(benchmarks[i], function (elapsed, code) {
     if (code != 0) {
-      console.log("ERROR  ");
+      console.log("ERROR  ")
     }
-    console.log(elapsed);
-    runNext(i+1);
-  });
-};
-runNext(0);
+    console.log(elapsed)
+    runNext(i+1)
+  })
+}
+
+runNext(0)
